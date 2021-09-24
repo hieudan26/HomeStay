28b28b5d753c0ec6317fe59b3caa53141acafcf7
maclife <maclife@MACLIFE1424>
2021-09-24 16:46:07 +0700

    Add project files.

---
+++ HomeStay/Class/My_DB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeStay
+{
+    class My_DB
+    {
+        // Change data source base on your computer
+        SqlConnection con = new SqlConnection("Data Source=(localdb)\\ProjectsV13;Initial Catalog=DB_HomeStay;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        public SqlConnection getConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+        public void openConnection()
+        {
+            if(con.State == ConnectionState.Closed)
+            {
+                con.Open();
+            }
+        }
+        public void closeConnection()
+        {
+            if(con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ HomeStay/Class/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HomeStay
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*My_DB mydb = new My_DB();
+            
+            if(IsServerConnected(mydb))
+            {
+                Console.WriteLine("Connected!");
+            }
+            else
+            {
+                Console.WriteLine("Not Yet Connected!");
+            }*/
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        private static bool IsServerConnected(My_DB mydb)
+        {
+            try
+            {
+                mydb.openConnection();
+                return true;
+            }
+            catch (SqlException)
+            {
+                    return false;
+             }
+        }
+    }
+}
